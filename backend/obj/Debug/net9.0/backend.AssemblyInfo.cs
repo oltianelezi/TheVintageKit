3ad2fcfd4392c39c133e89223d5d4558139cecb2
oltianelezi <oltianelezi1@gmail.com>
2025-09-09 21:51:20 +0200

    total price updates properly, api is called only once

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f0f2b3f9c9b3ddf4b4df6f87fa19e182b4bff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c7c8af75dd523a4c2e7630a40152aeb24e0b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
