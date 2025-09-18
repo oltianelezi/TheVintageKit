b6292ca516c496ff69474e316a5f9b7df527b438
oltianelezi <oltianelezi1@gmail.com>
2025-09-18 16:41:16 +0200

    full page for every product with full functionality

--- backend/Controllers/OrderController.cs
+++ backend/Controllers/OrderController.cs
@@ -15,24 +15,28 @@ namespace backend.Controllers
         private readonly OrderRepository _orderRepository;
         private readonly AddressRepository _addressRepository;
         private readonly OrderItemRepository _orderItemRepository;
+        private readonly ProductRepository _productRepository;
         private readonly EmailService _emailService;
 
         public OrderController(
             OrderRepository orderRepository,
             AddressRepository addressRepository,
             OrderItemRepository orderItemRepository,
-            EmailService emailService
+            EmailService emailService,
+            ProductRepository productRepository
             )
         {
             _orderRepository = orderRepository;
             _addressRepository = addressRepository;
             _orderItemRepository = orderItemRepository;
+            _productRepository = productRepository;
             _emailService = emailService;
         }
 
         [HttpPost]
         public async Task<IActionResult> NewOrder([FromBody] OrderRequest request)
         {
+            // Add shipping address to db
             var newAddress = new Address
             {
                 FirstName = request.Address.FirstName,
@@ -46,6 +50,7 @@ namespace backend.Controllers
 
             int newAddressId = await _addressRepository.CreateNewAddress(newAddress);
 
+            // Add new Order to db
             var newOrder = new Order
             {
                 TotalPrice = request.PaymentAmount,
@@ -56,6 +61,7 @@ namespace backend.Controllers
             int newOrderId = await _orderRepository.CreateOrder(newOrder);
 
             List<OrderItem> OrderList = new List<OrderItem>();
+            string OrderListEmail = string.Empty; // For the email order confirmation
 
             foreach (var item in request.Order)
             {
@@ -67,15 +73,23 @@ namespace backend.Controllers
                     Size = item.Size,
                     UnitPrice = item.UnitPrice
                 });
+
+                var productInfo = await _productRepository.GetProductById(item.ProductId);
+                OrderListEmail += productInfo.ProductName +
+                " | Size: " + item.Size
+                + " | Amount: " + item.Quantity
+                 + " | Price: $" + item.UnitPrice
+                 + "\n";
             }
 
-            var emailBody = $"Hello {newAddress.FirstName} {newAddress.LastName},\n\nYour order #{newOrderId} has been received!";
+            OrderListEmail += "Total Price: $" + newOrder.TotalPrice + "\n";
+
+            var emailBody = $"Hello {newAddress.FirstName} {newAddress.LastName},\n\nYour order: \n{OrderListEmail} has been received!";
 
             // Replace temp mail with NewOrder.Email
-            await _emailService.SendEmailAsync("yocacaw842@poesd.com", "Order Confirmation", emailBody);
+            await _emailService.SendEmailAsync("dikofi5216@dawhe.com", "Order Confirmation", emailBody);
 
             await _orderItemRepository.NewOrderItems(OrderList);
-
 
             return Ok();
         }
